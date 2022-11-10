9e9f347054dd2f739d9611deb89eb1132d355548
joridholmen <jorid.holmen@nmbu.no>
2022-11-10 10:53:18 +0100

    fix: fixed algorithm for creating path

--- CreatePath.cpp
+++ CreatePath.cpp
@@ -4,19 +4,17 @@ using namespace std;
 
 
 
-int CreatePath(int nodes) {
-    srand(time(0));
+int *CreatePath(int nodes, int len_path) {
     int start_node;
-    int len_path;
 
     while(true){
         start_node = rand() % nodes + 1; 
-        len_path = rand() % nodes + 1; 
+        //len_path = rand() % nodes + 1; 
         if (len_path != nodes) {break;}
     }
 
-    int path[len_path];
-    
+    int path[len_path];    
+
     if (start_node+len_path <= nodes) {
         int  end_node = start_node + len_path - 1;
         for (int i = start_node; i < end_node  + 1; ++i) {
@@ -42,21 +40,37 @@ int CreatePath(int nodes) {
     cout << "Start node: " << start_node << "\n";
     cout << "Length of path: " << len_path << "\n";
 
+    /*
     for (int i = 0; i < len_path; i++ ) {
         cout << path[i] << "\n";
     }
+    */
     
-    return 0;
+    return path;
 }
 
 
 
 int main() {
-
+    srand(time(0));
     int nodes = 10;
-    int p;
+    int len_path = rand() % nodes + 1; 
+    int *p;
 
-    p = CreatePath(nodes);
+    p = CreatePath(nodes, len_path);
+
+    int arr[len_path];
+    arr[0] = *p;
+
+    
+    for (int i=1; i <= len_path; i++) {
+        arr[i] = *(p + i);
+    }
+    
+
+    for (int i = 0; i < len_path; i++ ) {
+        cout << arr[i] << "\n";
+    }
 
     return 0;
 }
