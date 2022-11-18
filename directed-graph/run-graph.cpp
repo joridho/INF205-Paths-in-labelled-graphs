017b879383abf7564f39c7fd3056cbacbc5e87d4
christianie <christor@nmbu.no>
2022-11-18 15:23:57 +0100

    fix: modified run-graph.cpp and graph-benchmark.cpp

--- directed-graph/run-graph.cpp
+++ directed-graph/run-graph.cpp
@@ -6,6 +6,23 @@
 #include "graph.h"
 #include "query.h"
 
+// The main function:
+// The program terminates if the arguments are less than 3???
+// The program opens up the file as indata that the user puts as arguments**
+// If the user has not put a file, the program terminates and prints out an error message
+// Makes a object g of the Graph class
+// Stores graph in indata in g
+//closes file indata
+//
+// The program opens up the file as inquery that the user puts as arguments**
+// If the user has not put a file, the program terminates and prints out an error message
+// Makes a object q of the Query class
+// Stores query in inquery in q 
+// closes file inquery
+// apply query q to graph g
+// what happens then??
+//
+// // ./generate-graph <number of nodes> <query path length> <graph file> <query file>
 int main(int argc, char** argv)
 {
    assert(argc >= 3);
