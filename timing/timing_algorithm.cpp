cf311bc6728dd09795985da11c79418d99c975e1
joridholmen <jorid.holmen@nmbu.no>
2022-11-23 15:46:32 +0100

    fix: writing some information in file

--- timing/timing_algorithm.cpp
+++ timing/timing_algorithm.cpp
@@ -16,11 +16,12 @@ int main()
 
     while (q1)
     {
-        // lage en overskrift i filen 
-
         while (g1)
         {
+            // lage en overskrift i filen 
+
             // kjøre graph-benchmark 
+
             auto t0 = std::chrono::high_resolution_clock::now();
             // kjøre run-graph 
             auto t1 = std::chrono::high_resolution_clock::now();
@@ -31,11 +32,12 @@ int main()
 
     while (g2)
     {
-        // lage en overskrift i filen 
-
         while (q2)
         {
+            // lage en overskrift i filen 
+
             // kjøre graph-benchmark 
+
             auto t0 = std::chrono::high_resolution_clock::now();
             // kjøre run-graph 
             auto t1 = std::chrono::high_resolution_clock::now();
@@ -46,21 +48,33 @@ int main()
 
     // teste comparing paths 
 
-    // ny fil for comparing paths 
+    // manipulere results.dat
+    // results med {10, 100, 1000, 10000} paths som er funnet 
+    // results med 100 paths funnet, men {0, 0.25, 0.50, 0.75, 1} prosent er like 
+
+    // ny fil for comparing paths 1
+    // lage en overskrift i filen 
+    //fil liste_med_results {results10.dat, results100.dat, results1000.dat, results10000.dat}
     while (q1)
     {
-        // lage en overskrift i filen 
-
-        while (g1)
-        {
-            auto t0 = std::chrono::high_resolution_clock::now();
-            // kjøre comparing paths 
-            auto t1 = std::chrono::high_resolution_clock::now();
-            double t = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count() * 0.000000001;
-            // skrive t til filen 
-        }
+        auto t0 = std::chrono::high_resolution_clock::now();
+        // kjøre comparing paths 
+        auto t1 = std::chrono::high_resolution_clock::now();
+        double t = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count() * 0.000000001;
+        // skrive t til filen 
     }
 
+    // ny fil for comparing paths 2
+    // lage en overskrift i filen 
+    //fil liste_med_results {results0.dat, results25.dat, results50.dat, results75.dat, results100.dat}
+    while (q1)
+    {
+        auto t0 = std::chrono::high_resolution_clock::now();
+        // kjøre comparing paths 
+        auto t1 = std::chrono::high_resolution_clock::now();
+        double t = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count() * 0.000000001;
+        // skrive t til filen 
+    }
 
 
     // trenger en god ide på hvordan endre på graph-benchmark fra denne mappa 
@@ -77,19 +91,21 @@ int main()
 Hva skal måles:
 * tida det tar å sette sammen paths med forskjellig
     - antall noder på graph
-        10      q=5
-        100     q=5
-        1000    q=5
-        10000   q=5
-        100000  q=5
     - forskjellig query path length
-        10      g=10001
-        100     g=10001
-        1000    g=10001
-        10000   g=10001
     - disse to variert om hverandre 
+
 * tida det tar å sammenligne paths 
     - basert på hvor mange paths som er funnet
+        10
+        100
+        1000
+        10000
     - basert på hvor mange like paths det er 
+        0%      100
+        25%     100
+        50%     100
+        75%     100
+        100%    100
+        Om det ikke er en signifikant forskjell her er det ikke vits i å teste videre
 * korrelere noder, query length og antall paths funnet 
 */
